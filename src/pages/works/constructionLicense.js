8039e7d9089ba69618c62a23de6795ed83031c07
shuto uehara <shuto.u2f@gmail.com>
2022-10-29 21:40:34 +0900

    add works pages

--- src/pages/works/constructionLicense.js
+++ src/pages/works/constructionLicense.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
+import Layout from "../components/layout";
 
-export default function constructionLicense() {
+export default function constructionLicense () {
   return (
-    <>
+    <Layout>
       <h1>construction</h1>
       <Link to="/works/">受付業務一覧</Link>
-    </>
+    </Layout>
   )
 }
---
+++ src/pages/works/curio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+export default function curio () {
+  return (
+    <Layout>
+      <h1>construction</h1>
+      <Link to="/works/">受付業務一覧</Link>
+    </Layout>
+  )
+}
